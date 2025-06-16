b3e8a827ec6ce81983b0b3305f5a6620b4fc8b51
DiegoUzumaki2018 <diegouzumaki.44tormin@gmail.com>
2025-06-16 20:18:43 -0300

    Add files via upload

---
+++ exemplo5.py
@@ -0,0 +1,7 @@
+cont=int(0)
+for cont in range(0,5,2):
+        a=str(input("digite seu nome"))
+
+#repita o conteudo dentro do for fazendo com que o
+#contador comece no zero e vá até o 5
+#pulando de 2 em 2
