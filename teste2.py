8ee13f316847c1631b29ac10cf8dba936da7150f
DiegoUzumaki2018 <diegouzumaki.44tormin@gmail.com>
2025-07-03 21:04:57 -0300

    Add files via upload

---
+++ teste2.py
@@ -0,0 +1,16 @@
+import random
+valor=[0,0,0,0,0,0]
+a=random.randint(1,2)
+a=[0,0,0,0,0,0]
+for cont in range(6):
+    valor[cont]=int(input("Qual é o valor"))
+    if(valor[cont]==a[cont]):
+     
+     
+     
+     valor.append("venceu")
+
+
+
+    
+    
